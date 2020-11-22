6c007a5c1a337ab34a8102e53213f5259e3c169d
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-11-22 21:35:29 +0100

    Improve plugin code

---
+++ pythonscript_install_manager/addons.gd
@@ -0,0 +1,25 @@
+tool
+extends Object
+
+
+const Addon = preload("addon.gd")
+
+
+static func get_pythonscript_addon() -> Addon:
+	return Addon.new(
+		"pythonscript",
+		"Pythonscript",
+		"https://api.github.com/repos/touilleMan/godot-python/releases"
+	)
+
+
+static func get_install_manager_addon() -> Addon:
+	return Addon.new(
+		"pythonscript_install_manager",
+		"Install Manager",
+		"https://api.github.com/repos/touilleMan/godot-python-install-manager/releases"
+	)
+
+
+static func get_addons() -> Array:
+	return [get_install_manager_addon(), get_pythonscript_addon()]
