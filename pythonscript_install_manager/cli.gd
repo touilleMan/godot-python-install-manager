9830657567d13ec6366c8b0834efc764805e25a5
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-12-05 10:49:48 +0100

    wip

--- pythonscript_install_manager/cli.gd
+++ pythonscript_install_manager/cli.gd
@@ -3,7 +3,7 @@ extends SceneTree
 
 
 const Utils = preload("utils.gd")
-const Addon = preload("addon.gd")
+const Addons = preload("addons.gd")
 
 
 func _extract_script_args() -> Array:
@@ -12,79 +12,137 @@ func _extract_script_args() -> Array:
 	# This way we can simply look for the --script option as a delimiter
 	var args: Array = OS.get_cmdline_args()
 	var delimiter = max(args.find_last("--script"), args.find_last("-s"))
-	return args.slice(delimiter+1, len(args))
+	return args.slice(delimiter+2, len(args))
 
 
 func _print_usage():
-	var install_manager_addon = Utils.get_install_manager_addon()
-	print("""Godot-Python install manager version %s
-Usage: godot [--path <project_path>] [--no-window] --script res://addons/pythonscript_install_manager/cli.gd [options]
-self_upgrade [--version <VERSION>]: Upgrade the install manager
-install [--version <VERSION>]: Ensure Pythonscript is installed
-upgrade [--version <VERSION>]: Upgrade the installed version of Pythonscript
-""" % install_manager_addon.get_current_version(true))
+	var install_manager_addon = Addons.get_install_manager_addon()
+	print("""Usage: godot [--path <project_path>] [--no-window] --script res://addons/pythonscript_install_manager/cli.gd [options]
+info: Display installed versions info
+self_upgrade [<VERSION>]: Upgrade the install manager
+install [<VERSION>]: Ensure Pythonscript is installed
+upgrade [<VERSION>]: Upgrade the installed version of Pythonscript
+list_versions: List Pythonscript versions compatible with your OS/Godot version 
+self_list_versions: List install manager versions compatible with your OS/Godot version 
+""")
 
 
 func _parse_args(args: Array):
-	var version = "latest"
-	while args:
-		var arg = args.pop_front()
-		if arg == "--version":
-			version = args.pop_front()
-			if not version:
-				print("Argument --version must specify a SemVer value (e.g. `0.1.2`) or `latest`")
-				return null
-		else:
-			print("Unknown argument `%s`" % arg)
-			return null
-	return version
+	var raw_version = args.pop_front()
+	if args:
+		print("Too many arguments")
+		return null
+	if raw_version == null or raw_version == "latest":
+		return "latest"
+	if Utils.parse_version(raw_version):
+		return raw_version
+	else:
+		print("Invalid version: must specify a SemVer value (e.g. `0.1.2`) or `latest`")
+		return null
 
 
-func _cmd_self_upgrade(args: Array) -> int:
-	var version = _parse_args(args)
-	if not version:
-		return 1
-	var addon = Utils.get_install_manager_addon()
-	addon.upgrade_to_version(version)
+func _cmd_info() -> int:
+	for addon in Addons.get_addons():
+		var version = addon.get_current_version(true)
+		if version == null:
+			version = "Not installed"
+		print("%s: %s" % [addon.display_name, version])
 	return 0
 
 
-func _cmd_install(args: Array) -> int:
+func _cmd_self_upgrade(args: Array) -> int:
+	yield(Utils.noop_yield(), "completed")  # Ensure we return a coroutine no matter what
+
 	var version = _parse_args(args)
 	if not version:
 		return 1
-	var addon = Utils.get_pythonscript_addon()
-	var current_version = addon.get_current_version(true)
-	if current_version:
-		print("%s is already installed in version %s" % [addon.display_name, current_version])
-		return 0
-	else:
-		addon.upgrade_to_version("latest")
-		return 0
+	var addon = Addons.get_install_manager_addon()
+	return yield(_upgrade_addon(addon, version), "completed")
 
 
 func _cmd_upgrade(args: Array) -> int:
+	yield(Utils.noop_yield(), "completed")  # Ensure we return a coroutine no matter what
+
 	var version = _parse_args(args)
 	if not version:
 		return 1
-	var addon = Utils.get_pythonscript_addon()
-	addon.upgrade_to_version(version)
+	var addon = Addons.get_pythonscript_addon()
+	return yield(_upgrade_addon(addon, version), "completed")
+
+
+func _cmd_install(args: Array) -> int:
+	yield(Utils.noop_yield(), "completed")  # Ensure we return a coroutine no matter what
+
+	var version = _parse_args(args)
+	if not version:
+		return 1
+	var addon = Addons.get_pythonscript_addon()
+	var current_version = addon.get_current_version(true)
+	if current_version != null:
+		print("%s is already installed in version %s" % [addon.display_name, current_version])
+		return 0
+
+	else:
+		return yield(_upgrade_addon(addon, version), "completed")
+
+
+func _upgrade_addon(addon, version) -> int:
+	print("Fetching upgrade info...")
+	var ret
+	ret = yield(addon.fetch_upgrade_info(), "completed")
+	if ret[0] != OK:
+		print("Error: %s" % ret[1])
+		return 1
+
+	if version == "latest" and not addon.upgrade_needed():
+		print("Version %s is already the latest version" % addon.get_current_version(true))
+		return 0
+
+	else:
+		if version == "latest":
+			version = addon.get_latest_version(true)
+		print("Upgrading to version %s" % addon.get_latest_version(true))
+		ret = yield(addon.upgrade_to_version(version), "completed")
+		if ret[0] != OK:
+			print("Error: %s" % ret[1])
+			return 1
+
+	return 0
+
+
+func _list_addon_versions(addon) -> int:
+	yield(Utils.noop_yield(), "completed")  # Ensure we return a coroutine no matter what
+	print("Fetching update info...")
+	var ret
+	ret = yield(addon.fetch_upgrade_info(), "completed")
+	if ret[0] != OK:
+		print("Error: %s" % ret[1])
+		return 1
+	print("Available versions:")
+	for item in ret[1]:
+		print("%s (%s)" % [item["str_version"], item["archive_name"]])
 	return 0
 
 
 func _init():
 	var args: Array = _extract_script_args()
-	if not args or "--help" in args or "-h" or args:
+	if args.empty():
 		_print_usage()
 		OS.exit_code = 0
 	else:
 		var cmd = args.pop_front()
-		if cmd == "self_upgrade":
-			OS.exit_code = _cmd_self_upgrade(args)
+		if cmd == "info":
+			OS.exit_code = _cmd_info()
+		elif cmd == "self_upgrade":
+			OS.exit_code = yield(_cmd_self_upgrade(args), "completed")
 		elif cmd == "install":
-			OS.exit_code = _cmd_install(args)
+			OS.exit_code = yield(_cmd_install(args), "completed")
 		elif cmd == "upgrade":
-			OS.exit_code = _cmd_upgrade(args)
+			OS.exit_code = yield(_cmd_upgrade(args), "completed")
+		elif cmd == "list_versions":
+			OS.exit_code = yield(_list_addon_versions(Addons.get_pythonscript_addon()), "completed")
+		elif cmd == "self_list_versions":
+			OS.exit_code = yield(_list_addon_versions(Addons.get_install_manager_addon()), "completed")
 		else:
 			print("Unknown command `%s`" % cmd)
 			_print_usage()
--- pythonscript_install_manager/utils.gd
+++ pythonscript_install_manager/utils.gd
@@ -1,24 +1,179 @@
 extends Object
 
 
-const Addon = preload("addon.gd")
+const GDUnzip = preload("gdunzip.gd")
 
 
-static func get_install_manager_addon() -> Addon:
-	return Addon.new(
-		"pythonscript",
-		"Pythonscript",
-		"https://api.github.com/repos/touilleMan/godot-python/releases"
-	)
+# Stolen from GUT (MIT license Copyright (c) 2018 Tom "Butch" Wesley)
+static func get_root_node() -> Node:
+	var to_return = null
+	var main_loop = Engine.get_main_loop()
+	if(main_loop != null):
+		return main_loop.root
+	else:
+		push_error('No Main Loop Yet')
+		return null
 
 
-static func get_pythonscript_addon() -> Addon:
-	return Addon.new(
-		"pythonscript_install_manager",
-		"Install Manager",
-		"https://api.github.com/repos/touilleMan/godot-python-install-manager/releases"
-	)
+# Use noop_yield() at the begining of a function so caller have the
+# guarantee it will always be a coroutine function
+static func noop_yield():
+	yield(get_root_node().get_tree().create_timer(0), "timeout")
 
 
-static func get_addons() -> Array:
-	return [get_install_manager_addon(), get_pythonscript_addon()]
+static func http_request_factory() -> HTTPRequest:
+	var root = get_root_node()
+	var http_request = HTTPRequest.new()
+	http_request.use_threads = true
+	http_request.timeout = 30
+	root.add_child(http_request)
+	return http_request
+
+
+static func http_request_destructor(http_request: HTTPRequest):
+	var root = get_root_node()
+	root.remove_child(http_request)
+
+
+static func parse_version(str_version: String):
+	var version = []
+	for x in str_version.split('.'):
+		if x.is_valid_integer():
+			version.append(int(x))
+		else:
+			return null
+	if len(version) != 3:
+		return version
+	else:
+		return null
+
+
+static func is_more_recent_version(old: Array, new: Array) -> bool:
+	for item in range(3):
+		var old_item = old[item]
+		var new_item = new[item]
+		if old_item == new_item:
+			continue
+		else:
+			return old_item < new_item
+	return false  # old and new are the same version
+
+
+static func rmdir(path: String) -> int:
+	if OS.has_feature("Windows"):
+		path = path.replace("/", "\\")
+		return OS.execute("cmd.exe", ["/C", 'if exist "%s" ( del /Q /S /F "%s/*.*" && rmdir /Q /S "%s" )' % [path, path, path]])
+	else:
+		return OS.execute("sh", ["-c", "test -e '%s' && rm -rf '%s'" % [path, path]])
+
+
+static func rm(path: String) -> int:
+	if OS.has_feature("Windows"):
+		path = path.replace("/", "\\")
+		return OS.execute("cmd.exe", ["/C", 'if exist "%s" ( del "%s" )' % [path, path]])
+	else:
+		return OS.execute("sh", ["-c", "test -e '%s' && rm '%s'" % [path, path]])
+
+
+static func mv(old_path: String, new_path: String) -> int:
+	if OS.has_feature("Windows"):
+		old_path = old_path.replace("/", "\\")
+		new_path = new_path.replace("/", "\\")
+		return OS.execute("cmd.exe", ["/C", 'move "%s" "%s"' % [old_path, new_path]])
+	else:
+		return OS.execute("sh", ["-c", "mv '%s' '%s'" % [old_path, new_path]])
+
+
+static func unzip(zip_path: String, target_path: String, filter: String = "") -> int:
+	var gdunzip = GDUnzip.new()
+	var loaded = gdunzip.load(zip_path)
+	if loaded != true:
+		push_error("Cannot load zip file %s" % zip_path)
+		return FAILED
+
+	var filenames
+	if filter == "":
+		filenames = gdunzip.files.keys()
+	else:
+		var filter_regex = RegEx.new()
+		filter_regex.compile(filter)
+		filenames = []
+		for f in gdunzip.files:
+			var file_name = f["file_name"]
+			if filter_regex.search(file_name):
+				filenames.push_back(file_name)
+
+	# First create all needed directories
+	var directories = {}
+	for file_name in filenames:
+		var dir_path = "%s/%s" % [target_path, file_name.get_base_dir()]
+		directories[dir_path] = null
+	var dir = Directory.new()
+	for dir_path in directories.keys():
+		var make_dir_res = dir.make_dir_recursive(dir_path)
+		if make_dir_res != OK:
+			push_error("Cannot create directory `%s` from zip %s" % [dir_path, zip_path])
+			return FAILED
+
+	# Then extract all files
+	for file_name in filenames:
+		var uncompressed = gdunzip.uncompress(file_name)
+		if uncompressed == false:
+			push_error("Cannot uncompresse `%s` from zip %s" % [file_name, zip_path])
+			return FAILED
+		var file = File.new()
+		var file_path = "%s/%s" % [target_path, file_name]
+		var file_open_res = file.open(file_path, File.WRITE)
+		if file_open_res != OK:
+			push_error("Cannot create file %s: error %s" % [file_path, file_open_res])
+			return FAILED
+		file.store_buffer(uncompressed)
+		file.close()
+
+	return OK
+
+
+static func download(url: String, target_path: String = ""):
+	yield(noop_yield(), "completed")  # Ensure we return a coroutine no matter what
+
+	var http_request = http_request_factory()  # Don't forget to call destructor !
+	var error = http_request.request(url)
+	if error != OK:
+		var msg = "Cannot initiate HTTP request %s (error %s)" % [
+			url,
+			error
+		]
+		http_request_destructor(http_request)
+		return [FAILED, msg]
+
+	var vars = yield(http_request, "request_completed")
+	http_request_destructor(http_request)
+	var http_result = vars[0]
+	var status_code = vars[1]
+	var body = vars[3]
+
+	if http_result == HTTPRequest.RESULT_CANT_CONNECT or http_result == HTTPRequest.RESULT_CANT_RESOLVE:
+		var msg = "Cannot reach %s (error %s)" % [
+			url,
+			error
+		]
+		return [FAILED, msg]
+
+	elif http_result != OK or status_code != 200:
+		var msg = "Bad response from %s (status code: %s)\n%s" % [
+			url,
+			error,
+			body.get_string_from_utf8()
+		]
+		return [FAILED, msg]
+
+	if target_path:
+		var file = File.new()
+		var file_open_res = file.open(target_path, File.WRITE)
+		if file_open_res != OK:
+			var msg = "Cannot create file %s: error %s" % [target_path, file_open_res]
+			return [FAILED, msg]
+		file.store_buffer(body)
+		file.close()
+
+	return [OK, body]
